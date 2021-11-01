58b41759ec248f0b833113eb599cd61a47cb288f
池杰 <cj@baitutech.com>
2021-11-01 15:34:09 +0800

    init

--- baitu-common/baitu-common-core/src/main/java/com/baitu/cloud/constant/CacheConstants.java
+++
@@ -1,9 +0,0 @@
-package com.baitu.cloud.constant;
-
-/**
- * @author cj@baitutech.com
- * @since 2021.11.01 14:15
- * 缓存常量
- */
-public interface CacheConstants {
-}
--- baitu-common/baitu-common-core/src/main/java/com/baitu/cloud/constant/SecurityConstants.java
+++
@@ -1,9 +0,0 @@
-package com.baitu.cloud.constant;
-
-/**
- * @author cj@baitutech.com
- * @since 2021.11.01 14:16
- * 安全配置常量
- */
-public interface SecurityConstants {
-}
